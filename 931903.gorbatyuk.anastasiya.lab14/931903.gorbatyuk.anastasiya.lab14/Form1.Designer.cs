3c948875e06e90d71115b4a96627f29522afa6ec
nasty1326 <92375864+nasty1326@users.noreply.github.com>
2022-06-10 17:06:00 +0700

    Add files via upload

---
+++ 931903.gorbatyuk.anastasiya.lab14/931903.gorbatyuk.anastasiya.lab14/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace _931903.gorbatyuk.anastasiya.lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbPeople = new System.Windows.Forms.Label();
+            this.lbAllPeople = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numVhod = new System.Windows.Forms.NumericUpDown();
+            this.numOperator = new System.Windows.Forms.NumericUpDown();
+            this.numSer = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numVhod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOperator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numSer);
+            this.panel1.Controls.Add(this.numOperator);
+            this.panel1.Controls.Add(this.numVhod);
+            this.panel1.Controls.Add(this.btStop);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1137, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(25, 31);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(128, 38);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(185, 31);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(128, 38);
+            this.btStop.TabIndex = 1;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество людей в очереди:";
+            // 
+            // lbPeople
+            // 
+            this.lbPeople.AutoSize = true;
+            this.lbPeople.Location = new System.Drawing.Point(235, 161);
+            this.lbPeople.Name = "lbPeople";
+            this.lbPeople.Size = new System.Drawing.Size(20, 17);
+            this.lbPeople.TabIndex = 3;
+            this.lbPeople.Text = " 0";
+            // 
+            // lbAllPeople
+            // 
+            this.lbAllPeople.AutoSize = true;
+            this.lbAllPeople.Location = new System.Drawing.Point(133, 125);
+            this.lbAllPeople.Name = "lbAllPeople";
+            this.lbAllPeople.Size = new System.Drawing.Size(20, 17);
+            this.lbAllPeople.TabIndex = 5;
+            this.lbAllPeople.Text = " 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Всего людей:";
+            // 
+            // numVhod
+            // 
+            this.numVhod.Location = new System.Drawing.Point(1042, 47);
+            this.numVhod.Name = "numVhod";
+            this.numVhod.Size = new System.Drawing.Size(78, 22);
+            this.numVhod.TabIndex = 2;
+            // 
+            // numOperator
+            // 
+            this.numOperator.Location = new System.Drawing.Point(441, 47);
+            this.numOperator.Name = "numOperator";
+            this.numOperator.Size = new System.Drawing.Size(78, 22);
+            this.numOperator.TabIndex = 3;
+            // 
+            // numSer
+            // 
+            this.numSer.Location = new System.Drawing.Point(711, 47);
+            this.numSer.Name = "numSer";
+            this.numSer.Size = new System.Drawing.Size(78, 22);
+            this.numSer.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Операторов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(556, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Сервисный параметр";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(811, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Параметр для входящего потока";
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Location = new System.Drawing.Point(292, 116);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series5.ChartArea = "ChartArea1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(828, 431);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 559);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lbAllPeople);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbPeople);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numVhod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOperator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numSer;
+        private System.Windows.Forms.NumericUpDown numOperator;
+        private System.Windows.Forms.NumericUpDown numVhod;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbPeople;
+        private System.Windows.Forms.Label lbAllPeople;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 931903.gorbatyuk.anastasiya.lab14/931903.gorbatyuk.anastasiya.lab14/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _931903.gorbatyuk.anastasiya.lab14
+{
+    public partial class Form1 : Form
+    {
+
+        public abstract class Agent
+        {
+            public virtual double getNextEventTime()
+            {
+                return double.MaxValue;
+            }
+            public virtual void processEvent()
+            {
+
+            }
+        }
+        public class ArrivalProcess : Agent
+        {
+            private Random rnd = new Random();
+            private double nextArrivalTime = 0;
+            private Bank bank;
+            public ArrivalProcess(Bank bank)
+            {
+                this.bank = bank;
+                nextArrivalTime = simulateInterrivalTime();
+            }
+            private double simulateInterrivalTime()
+            {
+                return -Math.Log(rnd.NextDouble()) / Model.lArr;
+            }
+            public override double getNextEventTime()
+            {
+                return nextArrivalTime;
+            }
+            public override void processEvent()
+            {
+                Customer customer = new Customer();
+                bank.customerArrived(customer);
+                nextArrivalTime += simulateInterrivalTime();
+            }
+        }
+        public class Customer : Agent
+        {
+
+        }
+        public class Bank : Agent
+        {
+            private Service service = new Service(Model.opsAmount);
+            private MyQueue queue = new MyQueue();
+            public void customerArrived(Customer customer)
+            {
+                if (service.hasFreeOperator()) service.acceptCustomer(customer);
+                else queue.acceptCustomer(customer);
+            }
+            public override double getNextEventTime()
+            {
+                return service.getNextEventTime();
+            }
+            public override void processEvent()
+            {
+                service.processEvent();
+                if (queue.hasCustomers())
+                {
+                    Customer cus = queue.takeCustomer();
+                    service.acceptCustomer(cus);
+                }
+            }
+            internal int getBusyOperatorsSize()
+            {
+                return service.getBusyOperatorsSize();
+            }
+            public int getQueueSize()
+            {
+                return queue.getQueueSize();
+            }
+        }
+        public class MyQueue : Agent
+        {
+            private Queue<Customer> queue = new Queue<Customer>();
+            public void acceptCustomer(Customer customer)
+            {
+                queue.Enqueue(customer);
+            }
+            public bool hasCustomers()
+            {
+                return (queue.Count > 0);
+            }
+            public Customer takeCustomer()
+            {
+                return queue.Dequeue();
+            }
+            public int getQueueSize()
+            {
+                return queue.Count();
+            }
+        }
+        public class Service : Agent
+        {
+            private List<Operator> operators = new List<Operator>();
+            private Operator activeOper = new Operator();
+            public Service(int N)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    operators.Add(new Operator());
+                }
+            }
+            public void acceptCustomer(Customer customer)
+            {
+                Operator op = findFreeOperator();
+                if (op != null) op.acceptCustomer(customer);
+            }
+            public bool hasFreeOperator()
+            {
+                Operator op = findFreeOperator();
+                return (op != null);
+            }
+            private Operator findFreeOperator()
+            {
+                foreach (Operator op in operators)
+                    if (op.isFree()) return op;
+                return null;
+            }
+            public override double getNextEventTime()
+            {
+                double tMin = double.MaxValue;
+                activeOper = null;
+                foreach (Operator op in operators)
+                {
+                    double tA = op.getNextEventTime();
+                    if (tA < tMin)
+                    {
+                        tMin = tA;
+                        activeOper = op;
+                    }
+                }
+                return tMin;
+            }
+            public override void processEvent()
+            {
+                if (activeOper != null)
+                    activeOper.processEvent();
+            }
+            public int getBusyOperatorsSize()
+            {
+                int size = 0;
+                foreach (Operator op in operators)
+                    if (!op.isFree())
+                        size++;
+                return size;
+            }
+        }
+        public class Operator : Agent
+        {
+            private Customer customerInService = null;
+            private double endOfSeviceTime = double.MaxValue;
+            private Random rnd = new Random();
+            public void acceptCustomer(Customer customer)
+            {
+                if (isFree())
+                {
+                    customerInService = customer;
+                    endOfSeviceTime = Model.Time + simulateServiceTime();
+                }
+            }
+            private double simulateServiceTime()
+            {
+                return -Math.Log(rnd.NextDouble()) / Model.lOper;
+            }
+            public bool isFree()
+            {
+                return (customerInService == null);
+            }
+            public override double getNextEventTime()
+            {
+                return endOfSeviceTime;
+            }
+            public override void processEvent()
+            {
+                customerInService = null;
+                endOfSeviceTime = double.MaxValue;
+            }
+        }
+
+            public static class Model
+        {
+            private static List<Agent> agents = new List<Agent>();
+            private static Bank bank;
+            private static ArrivalProcess arrivalProcess;
+            private static double T = 0;
+            public static double Time { get { return T; } }
+            private static Agent activeAgent;
+            private static int customersAmount;
+            public static int CAmount { get { return customersAmount; } }
+            private static double lambdaArrival = 0;
+            public static double lArr { get { return lambdaArrival; } }
+            private static double lambdaOperator = 0;
+            public static double lOper { get { return lambdaOperator; } }
+            private static int operatorsAmount = 0;
+            public static int opsAmount { get { return operatorsAmount; } }
+
+            public static void Run(double lambdaArr, double lambdaOper, int opsAmount)
+            {
+                lambdaArrival = lambdaArr;
+                lambdaOperator = lambdaOper;
+                operatorsAmount = opsAmount;
+                bank = new Bank();
+                arrivalProcess = new ArrivalProcess(bank);
+                agents.Add(bank);
+                agents.Add(arrivalProcess);
+                T = 0;
+            }
+            public static bool Iter()
+            {
+                if (continueCondition(T, activeAgent))
+                {
+                    double tMin = double.MaxValue;
+                    activeAgent = null;
+                    foreach (Agent agent in agents)
+                    {
+                        double tAgent = agent.getNextEventTime();
+                        if (tAgent < tMin)
+                        {
+                            tMin = tAgent;
+                            activeAgent = agent;
+                        }
+                    }
+                    T = tMin;
+                    if (activeAgent != null) activeAgent.processEvent();
+                    customersAmount++;
+                    return true;
+                }
+                return false;
+            }
+            public static bool continueCondition(double t, Agent activeAgent)
+            {
+                return (t < 100);
+            }
+            public static int queueSize()
+            {
+                return bank.getQueueSize();
+            }
+            public static int getBusyOperatorsSize()
+            {
+                return bank.getBusyOperatorsSize();
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+
+            chart1.Series[0].Points.Clear();
+            label4.Text = "";
+            Model.Run((double)numVhod.Value, (double)numSer.Value, (int)numOperator.Value);
+            chart1.Series[0].Points.AddXY(0, 0);
+            busyOps = Model.getBusyOperatorsSize();
+            timer1.Enabled = true;
+        }
+
+        int busyOps = 0;
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = Model.Iter();
+            if (busyOps < Model.getBusyOperatorsSize())
+            {
+                chart1.Series[0].Points.AddXY(Model.Time, busyOps);
+                busyOps++;
+                chart1.Series[0].Points.AddXY(Model.Time, busyOps);
+            }
+            else
+            {
+                chart1.Series[0].Points.AddXY(Model.Time, busyOps);
+                busyOps--;
+                chart1.Series[0].Points.AddXY(Model.Time, busyOps);
+            }
+            lbPeople.Text = Model.queueSize().ToString();
+            lbAllPeople.Text = Model.CAmount.ToString();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
